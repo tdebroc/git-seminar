70176880059d0218e3deb1cb0e497331c8d0e5fa
thibautdebroca <tdebroc@gmail.com>
2020-11-06 11:51:29 +0100

    Fix Format Code

--- src/com/isep/tp1/Main.java
+++ src/com/isep/tp1/Main.java
@@ -10,11 +10,11 @@ public class Main {
         pistes.add(new Piste(5 * 60, "I will Survive."));
         pistes.add(new Piste(3 * 60, "Bande Organisée."));
         pistes.add(new Piste(4 * 60, "Hot Stuff."));
-        pistes.add(new  Piste(2 * 60, "Zumba."));
-        pistes.add(new  Piste(7 * 60, "Billy Jean"));
-        pistes.add(new  Piste(7 * 60, "Lola"));
-        pistes.add(new  Piste(7 * 60, "Bella Ciao"));
-        pistes.add(new Piste (4 * 60, "Dance on the Mars")) ;
+        pistes.add(new Piste(2 * 60, "Zumba."));
+        pistes.add(new Piste(7 * 60, "Billy Jean"));
+        pistes.add(new Piste(7 * 60, "Lola"));
+        pistes.add(new Piste(7 * 60, "Bella Ciao"));
+        pistes.add(new Piste(4 * 60, "Dance on the Mars"));
 
 
         for (Piste current : pistes) {
